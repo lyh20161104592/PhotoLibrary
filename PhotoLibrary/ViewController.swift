15c010ac9f70eeb2b76ba280dfb8765522179fb9
20131104592 <lyh@admindeiMac-62.local>
2019-01-06 18:50:27 +0800

    1
    1

--- PhotoLibrary/ViewController.swift
+++ PhotoLibrary/ViewController.swift
@@ -6,15 +6,61 @@
 //  Copyright © 2018 LYH. All rights reserved.
 //
 
+
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate,
+UINavigationControllerDelegate {
+    
+    @IBOutlet weak var EditSwitch: UISwitch!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    //选取相册
+    @IBAction func openphoto(_ sender: Any) {
 
-
+        //判断设置是否支持图片库
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
+            //初始化图片控制器
+            let picker = UIImagePickerController()
+            //设置代理
+            picker.delegate = self
+            //指定图片控制器类型
+            picker.sourceType = UIImagePickerController.SourceType.photoLibrary
+            //设置是否允许编辑
+            picker.allowsEditing = EditSwitch.isOn
+            //弹出控制器，显示界面
+            self.present(picker, animated: true, completion: {
+                () -> Void in
+            })
+        }else{
+            print("读取相册错误")
+        }
+        
+    }
+    //选择图片成功后代理
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        //查看info对象
+        print(info)
+        //显示的图片
+        let image:UIImage!
+        if EditSwitch.isOn {
+            //获取编辑后的图片
+            image = info[.editedImage] as? UIImage
+        }else{
+            //获取选择的原图
+            image = info[.originalImage] as? UIImage
+        }
+        
+        imageView.image = image
+        //图片控制器退出
+        picker.dismiss(animated: true, completion: {
+            () -> Void in
+        })
+    }
 }
-
